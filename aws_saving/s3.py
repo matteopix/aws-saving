4d6cea55bca58bacc55c2f9f942562d20610c4ab
Matteo Piombo <matteo.piombo@pixartprinting.com>
2020-08-18 11:03:18 +0200

    S3: manage no tags case

--- aws_saving/s3.py
+++ aws_saving/s3.py
@@ -19,6 +19,7 @@ Here's an example:
 """
 
 import boto3
+from botocore.errorfactory import ClientError
 from .service import Service
 
 class S3(Service):
@@ -40,7 +41,13 @@ class S3(Service):
         instances_list = self.s3.list_buckets()
         instances = []
         for instance in instances_list['Buckets']:
-            tag_list = self.s3.get_bucket_tagging(Bucket=instance['Name'])
+            try:
+                tag_list = self.s3.get_bucket_tagging(Bucket=instance['Name'])
+            except ClientError as e:
+                if e.response['Error']['Code'] == 'NoSuchTagSet':
+                    continue # No tags
+                else:
+                    raise e
             saving = self.get_value(tag_list['TagSet'], 'saving')
             if saving and saving.lower() == 'enabled':
                 instance['DeletionProtection'] = True
